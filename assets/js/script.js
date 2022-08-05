d79b2c5504bc42d0db67e5017037b91ab20c890b
Diego <diegopena2022@gmail.com>
2022-08-04 17:24:57 -0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,10 +10,9 @@ var btn1 = document.getElementById("btn1")
 var btn2 = document.getElementById("btn2")
 var btn3 = document.getElementById("btn3")
 var btn4 = document.getElementById("btn4")
-var highscore = document.getElementById("user-name")
+var user = document.getElementById("user-name")
 var submit = document.getElementById("submit")
-
-
+var userArr = []
 
 function startTimer() {
     timeInterval = setInterval(function() {
@@ -33,15 +32,68 @@ function startGame() {
             console.log(event)
             option1.textContent = "false"
             time = time - 10;
-        } if (question.textContent === "What is the 7th letter in the alphabet"){
+        } else if (question.textContent === "What is the 7th letter in the alphabet"){
             event.preventDefault()
             option1.textContent = "true"
             question3();
-        } if (question.textContent === "How many months are in a year?"){
+        } else if (question.textContent === "How many months are in a year?"){
             event.preventDefault()
             console.log(event)
             option1.textContent = "false"
             time = time - 10     
+        }
+    });
+
+    btn2.addEventListener("click", function(event) {
+        if(question.textContent === "How many days are in a week?"){
+            event.preventDefault()
+            console.log(event)
+            option2.textContent = "false"
+            time = time - 10;
+        } else if (question.textContent === "What is the 7th letter in the alphabet"){
+            event.preventDefault()
+            option2.textContent = "false"
+            time = time - 10;
+        } else if (question.textContent === "How many months are in a year?"){
+            event.preventDefault()
+            console.log(event)
+            option2.textContent = "false"
+            time = time - 10     
+        }
+    });
+
+    btn3.addEventListener("click", function(event) {
+        if(question.textContent === "How many days are in a week?"){
+            event.preventDefault()
+            console.log(event)
+            option3.textContent = "false"
+            time = time - 10;
+        } else if (question.textContent === "What is the 7th letter in the alphabet"){
+            event.preventDefault()
+            option3.textContent = "false"
+            time = time - 10;
+        } else if (question.textContent === "How many months are in a year?"){
+            event.preventDefault()
+            console.log(event)
+            option3.textContent = "false"
+            time = time - 10     
+        }
+    });
+
+    btn4.addEventListener("click", function(event) {
+        if(question.textContent === "How many days are in a week?"){
+            event.preventDefault()
+            console.log(event)
+            option4.textContent = "true"
+            question2();
+        } else if (question.textContent === "What is the 7th letter in the alphabet"){
+            event.preventDefault()
+            option4.textContent = "false"
+            time = time - 10;
+        } else if (question.textContent === "How many months are in a year?"){
+            event.preventDefault()
+            option4.textContent = "true"
+            endQuiz()   
         }
     });
     
@@ -49,30 +101,15 @@ function startGame() {
     btn2.style.visibility = "visible";
     btn3.style.visibility = "visible";
     btn4.style.visibility = "visible";
-    
     question1()
+    
     function question1() {
     question.textContent = "How many days are in a week?"
     option1.textContent = "3" 
     option2.textContent = "8"
     option3.textContent = "6"
     option4.textContent = "7"
-
-
-    btn2.addEventListener("click", function(event) {
-    option2.textContent = "false"
-     time = time - 10
-    });
-
-    btn3.addEventListener("click", function(event) {
-    option3.textContent = "false"
-    time = time - 10
-    });
-
-    btn4.addEventListener("click", function(event) {
-    option4.textContent = "true"
-    question2() 
-    });
+    };
     }
 
     function question2() {
@@ -85,27 +122,6 @@ function startGame() {
         option2.textContent = "t"
         option3.textContent = "j"
         option4.textContent = "q"
-    
-        
-            btn2.addEventListener("click", function() {
-
-            option2.textContent = "false"
-             time = time - 10
-             
-            });
-        
-            btn3.addEventListener("click", function() {
-                debugger;
-            option3.textContent = "false"
-            time = time - 10
-            
-            });
-        
-            btn4.addEventListener("click", function() {
-                debugger;
-            option4.textContent = "false"
-            time = time - 10
-            });
     }
     
     function question3() {
@@ -118,29 +134,11 @@ function startGame() {
         option2.textContent = "8"
         option3.textContent = "11"
         option4.textContent = "12"
-    
-        
-            btn2.addEventListener("click", function() {
-            option2.textContent = "false"
-             time = time - 10
-             
-            });
-        
-            btn3.addEventListener("click", function() {
-            option3.textContent = "false"
-            time = time - 10
-            
-            });
-        
-            btn4.addEventListener("click", function() {
-            option4.textContent = "true"
-            endQuiz()
-            });
     }
 
     function endQuiz() {
-        question.textContent = "Enter Username Below?"
-        option1.textContent = "" 
+        question.textContent = "Your score is " + time
+        option1.textContent = ""  
         option2.textContent = ""
         option3.textContent = ""
         option4.textContent = ""
@@ -151,9 +149,11 @@ function startGame() {
         btn3.style.visibility = "hidden";
         btn4.style.visibility = "hidden";
         highscore.style.visibility = "visible"
-        console.log(highscore)
-        var userScore = localStorage.setItem("highscores", JSON.stringify({ initials: "dp", highscore }))
-        console.log(userScore)
+        stopTimer()
+
+        function stopTimer() {
+            clearInterval(timeInterval)
+        }
 
         submit.addEventListener("click", function() {
             board()
@@ -174,13 +174,9 @@ function startGame() {
         btn4.style.visibility = "hidden";
         highscore.style.visibility = "hidden"
 
-        var highscores = localStorage.getItem("highscores")
 
-        if (highscores) {
-            console.log(highscores)
-        }
     }
-}
+
 
 
 button.addEventListener("click", function() {
